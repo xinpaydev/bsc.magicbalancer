96111081e0b17ee270aa41f9e646b1c8d9dd2a3d
WizardofDeFi <79704561+WizardofDeFi@users.noreply.github.com>
2021-04-10 18:24:12 +0300

    Added bsc-magicbalancer frontend

---
+++ js/chunk-2d0c2307.d8740ab7.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c2307"],{"48f3":function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("About")])},c=[],l={name:"About"},o=l,a=t("2877"),r=Object(a["a"])(o,u,c,!1,null,null,null);e["default"]=r.exports}}]);
